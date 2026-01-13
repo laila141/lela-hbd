f2bb68cbeca355c05c87b5278cd490fabc033531
paesol <102522046+faisalekasyahputra@users.noreply.github.com>
2026-01-13 16:17:05 +0700

    aaa

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -3,9 +3,22 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [],
+  "polyfillFiles": [
+    "static/chunks/56d89_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
   "lowPriorityFiles": [],
-  "rootMainFiles": []
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_536872b7._.js",
+    "static/chunks/56d89_next_dist_compiled_react-dom_45f971a1._.js",
+    "static/chunks/56d89_next_dist_compiled_react-server-dom-turbopack_16f8209f._.js",
+    "static/chunks/56d89_next_dist_compiled_next-devtools_index_a836ccdc.js",
+    "static/chunks/56d89_next_dist_compiled_e59235c8._.js",
+    "static/chunks/56d89_next_dist_client_d4a9f7af._.js",
+    "static/chunks/56d89_next_dist_0755d513._.js",
+    "static/chunks/56d89_@swc_helpers_cjs_36159abd._.js",
+    "static/chunks/lela-hbd_a0ff3932._.js",
+    "static/chunks/turbopack-lela-hbd_70b8b041._.js"
+  ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
